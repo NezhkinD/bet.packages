64345334a345f53dc5114d1c90009997704e75c4
nezhkind <dnezhkin@gmail.com>
2021-01-05 16:41:28 +0300

    050121-1640: add MongoCollectionsEnum.php

---
+++ microservices/NosqlDatabaseClusterMicroservice/Enum/MongoCollectionsEnum.php
@@ -0,0 +1,29 @@
+<?php
+namespace BetPackages\Microservices\NosqlDatabaseClusterMicroservice\Enum;
+
+
+class MongoCollectionsEnum
+{
+    private const TEAM_STANDARDIZER_COLLECTION = "team-standardizer";
+
+
+    private string $collectionName;
+
+    /**
+     * @return string
+     */
+    public function getCollectionName(): string
+    {
+        return $this->collectionName;
+    }
+
+    /**
+     * @param string $collectionName
+     * @return MongoCollectionsEnum
+     */
+    public function setCollectionName(string $collectionName): MongoCollectionsEnum
+    {
+        $this->collectionName = $collectionName;
+        return $this;
+    }
+}
